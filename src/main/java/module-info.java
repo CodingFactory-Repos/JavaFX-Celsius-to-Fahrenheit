2d23f82e1d99017bd714b7acbe371b74770c4d36
Louis <35641311+Loule95450@users.noreply.github.com>
2021-11-30 13:32:22 +0100

    🦴 - Add JavaFX Template

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module me.loule.celsiustofahrenheit {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens me.loule.celsiustofahrenheit to javafx.fxml;
+    exports me.loule.celsiustofahrenheit;
+}
